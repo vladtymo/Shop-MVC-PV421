bf9e0daa62ff5802bb7a49c59cf81ddb71220c17
Vlad Tymoshchuk <tymo.vlad@gmail.com>
2025-08-22 19:26:32 +0300

    Add project files.

---
+++ Shop_mvc_pv421/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+using Microsoft.AspNetCore.Mvc;
+using Shop_mvc_pv421.Models;
+using System.Diagnostics;
+
+namespace Shop_mvc_pv421.Controllers
+{
+    public class HomeController : Controller
+    {
+        public HomeController()
+        {
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            // do logic here if needed
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Shop_mvc_pv421/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Shop_mvc_pv421.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
