62afe5f17d6c7b2ee234ae3299f46f794198a505
Vlad Tymoshchuk <tymo.vlad@gmail.com>
2025-09-03 20:31:16 +0300

    Create partial view for confirm modal dialog

---
+++ Shop_mvc_pv421/Models/ConfirmDialogModel.cs
@@ -0,0 +1,18 @@
+﻿namespace Shop_mvc_pv421.Models
+{
+    public enum DialogType
+    {
+        danger,
+        info,
+        success,
+        warning
+    }
+    public class ConfirmDialogModel
+    {
+        public string DialogId { get; set; }
+        public string? Title { get; set; }
+        public string? Message { get; set; }
+        // danger, warning, info, success
+        public DialogType Mode { get; set; } = DialogType.info;
+    }
+}
--- Shop_mvc_pv421/wwwroot/js/site.js
+++ Shop_mvc_pv421/wwwroot/js/site.js
@@ -2,3 +2,15 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+function configConfirmModal(dialogId, confirmUrl) {
+    var modal = document.getElementById(dialogId)
+    var deleteBtn = modal.querySelector('.actionBtn')
+
+    modal.addEventListener('show.bs.modal', function (event) {
+        // Button that triggered the modal
+        var button = event.relatedTarget
+
+        var id = button.getAttribute('data-bs-id')
+        deleteBtn.setAttribute('href', confirmUrl + id)
+    })
+}
